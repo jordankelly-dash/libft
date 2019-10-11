e22d3b0e3932fff201d73e7f2f7f012ca60cc4f0
jordankelly-dash <56453216+jordankelly-dash@users.noreply.github.com>
2019-10-11 12:44:45 -0700

    Add files via upload

--- ft_strncpy.c
+++ ft_strncpy.c
@@ -6,7 +6,7 @@
 /*   By: jkelly- <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/16 19:26:15 by jkelly-           #+#    #+#             */
-/*   Updated: 2019/10/10 19:40:57 by jkelly-          ###   ########.fr       */
+/*   Updated: 2019/10/11 12:31:29 by jkelly-          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ char	*ft_strncpy(char *dest, char *src, size_t len)
 
 	i = 0;
 	if (!len)
-		return ((char*)src);
+		return ((char*)dest);
 	while (src[i] && i < len)
 	{
 		dest[i] = src[i];
