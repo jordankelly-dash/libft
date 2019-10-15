f78311e4edfa678a0e72dd4dc451f80814b7745d
jordankelly-dash <56453216+jordankelly-dash@users.noreply.github.com>
2019-10-14 19:40:50 -0700

    Add files via upload

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,16 +6,18 @@
 /*   By: jkelly- <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/16 13:23:03 by jkelly-           #+#    #+#             */
-/*   Updated: 2019/10/14 19:09:22 by jkelly-          ###   ########.fr       */
+/*   Updated: 2019/10/14 19:39:12 by jkelly-          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int		ft_atoi(const char *str)
 {
-	long long int sign;
-	long long int val;
+	int sign;
+	int val;
+	int overflow;
 
 	sign = 1;
+	overflow = 0;
 	val = 0;
 	while (*str == 32 || (*str >= 9 && *str <= 13))
 	{
@@ -29,8 +31,11 @@ int		ft_atoi(const char *str)
 	}
 	while (*str >= '0' && *str <= '9')
 	{
-		val = (val * 10) + (long long int)(*str - '0');
+		val = (val * 10) + (*str - '0');
 		str++;
+		overflow++;
+		if (overflow >= 19)
+			return ((sign == -1) ? 0 : (-1));
 	}
 	return (val * sign);
 }
